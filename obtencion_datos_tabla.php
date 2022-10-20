ef4e1c5561ea8704198feb0d6dcd22e70e054148
Aldo6890 <aldocastillos433@gmail.com>
2022-10-19 22:10:33 -0500

    Solucione los id y cree un archivo php con la obtencion de los datos de la tabla.

---
+++ obtencion_datos_tabla.php
@@ -0,0 +1,25 @@
+<?php //Ejemplo aprenderaprogramar.com
+
+$programa = $_POST['progama_expo'];
+
+$hora = $_POST['hora_expo'];
+
+$actividad = $_POST['actividad_expo'];
+
+$tema = $_POST['tema_expo'];
+
+$expositor=$_POST['Expositor_expo'];
+
+$acesor=$_POST['asesor_expo'];
+
+$materia=$_POST['materia_expo'];
+
+echo $programa;
+echo $hora;
+echo $actividad;
+echo $tema;
+echo $expositor;
+echo $acesor;
+echo $materia;
+
+?>
